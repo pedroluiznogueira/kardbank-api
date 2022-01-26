7958046727ded989d90f196c6f8ee241ad8949ad
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-25 23:50:56 -0300

    first commit

---
+++ src/main/java/com/github/pedroluiznogueira/kardbankapi/KardbankApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.kardbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardbankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KardbankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pedroluiznogueira/kardbankapi/KardbankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.kardbankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KardbankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
