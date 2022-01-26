714021eb2674bedd283aa87f1997395005b445b4
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-26 13:12:12 -0300

    persisting files in aws s3

--- src/main/java/com/github/pedroluiznogueira/kardbankapi/domain/entity/User.java
+++ src/main/java/com/github/pedroluiznogueira/kardbankapi/domain/entity/User.java
@@ -3,14 +3,12 @@ package com.github.pedroluiznogueira.kardbankapi.domain.entity;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @RequiredArgsConstructor
+@Table(name = "user_table")
 public class User {
 
     @Id
